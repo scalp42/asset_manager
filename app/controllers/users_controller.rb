cbb321f6aa5f8c0c1f4f11078e0920b2356d55cd
Gurpreet Parmar <gparmar@blackboard.com>
2012-08-31 17:37:39 -0700

    setup create user section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,5 +25,20 @@ class UsersController < ApplicationController
 
   def create
 
+    user = User.new()
+
+    user.first_name = params['first_name']['first_name']
+    user.last_name = params['last_name']['last_name']
+    user.full_name = params['first_name']['first_name']+' '+params['last_name']['last_name']
+    user.email = params['email']['email']
+    user.password = params['password']['password']
+    user.password_confirmation = params['password_confirmation']['password_confirmation']
+
+    user.active = true
+
+    if user.save
+      redirect_to :back
+    end
+
   end
 end
