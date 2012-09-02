8c59840900c8160e12ef6d900afc571e0eed6409
Gurpreet Parmar <gparmar@blackboard.com>
2012-09-02 10:03:27 -0700

    setup create user api using rest

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,7 @@ class Api::UsersController < Api::BaseController
   before_filter :authenticate_user!
   before_filter :ensure_params_exist, :except => [:index]
   before_filter :ensure_email_params_exist, :except => [:index]
+  before_filter :ensure_password_params_exist, :except => [:index]
   respond_to :json
 
   def index
@@ -9,6 +10,24 @@ class Api::UsersController < Api::BaseController
   end
 
   def create
+
+    user = User.new(:email => params[:user][:email],:password => params[:user][:password],:password_confirmation => params[:user][:password],:active => true)
+
+    if params[:user][:first_name] != nil
+      user.first_name = params[:user][:first_name]
+    end
+
+    if params[:user][:last_name] != nil
+      user.last_name = params[:user][:last_name]
+    end
+
+    if user.first_name != nil and user.last_name != nil
+      user.full_name = user.first_name+" "+user.last_name
+    end
+
+    if user.save
+      render :json => {:success => true, :id => user.id}
+    end
 
   end
 
