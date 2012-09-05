a6adc1e368ad146c633277871720409320c1627d
Gurpreet Parmar <parmarg@gmail.com>
2012-09-05 14:14:09 -0700

    forced security creation with asset type

--- app/controllers/security_controller.rb
+++ app/controllers/security_controller.rb
@@ -9,7 +9,7 @@ class SecurityController < ApplicationController
   end
 
   def create
-    security = SecurityScheme.new(:name => params[:name][:name],:description => params[:description][:description])
+    security = SecurityScheme.new(:name => params[:name][:name],:description => params[:description][:description],:asset_type_id => BSON::ObjectId.from_string(params[:asset_type][:asset_type]))
 
     if security.save
       @securities = SecurityScheme.all
