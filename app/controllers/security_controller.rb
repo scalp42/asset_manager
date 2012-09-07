97a4c2fedcc35fec152b0d86ff8ff7103228a43b
Gurpreet Parmar <parmarg@gmail.com>
2012-09-06 20:57:18 -0700

    setup cancan to read permissions from security scheme

--- app/controllers/security_controller.rb
+++ app/controllers/security_controller.rb
@@ -33,10 +33,13 @@ class SecurityController < ApplicationController
     users = Array.new
     users.push(params[:users][:users])
 
+    puts params.inspect
+    puts 'jsdfkjskldfjkldsj'
     case params[:security_type][:security_type]
       when 'create'
         @security.create_restrictions['users'] = users
-      when 'edit '
+      when 'edit'
+        puts 'kjsdlfkjsdklfjklsdjf'
         @security.edit_restrictions['users'] = users
       when 'view'
         @security.view_restrictions['users'] = users
@@ -48,9 +51,30 @@ class SecurityController < ApplicationController
       securityReturn(@security.name,"Restrictions Updated")
       render :template => 'security/view_restrictions'
     end
+
   end
 
   def add_remove_groups
 
+    @security =  SecurityScheme.find(BSON::ObjectId.from_string(params[:security_id][:security_id]))
+
+    groups = Array.new
+    groups.push(params[:groups][:groups])
+
+    case params[:security_type][:security_type]
+      when 'create'
+        @security.create_restrictions['groups'] = groups
+      when 'edit'
+        @security.edit_restrictions['groups'] = groups
+      when 'view'
+        @security.view_restrictions['groups'] = groups
+      when 'delete'
+        @security.delete_restrictions['groups'] = groups
+    end
+
+    if @security.save
+      securityReturn(@security.name,"Restrictions Updated")
+      render :template => 'security/view_restrictions'
+    end
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,27 +2,38 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    user ||= User.new
+
+    SecurityScheme.all.each do |security|
+      security.create_restrictions[:users].each do |privilegedUsers|
+        privilegedUsers.each do |privilegedUser|
+          if privilegedUser == user.id.to_s
+            can :create, Asset, :asset_type_id => security.asset_type_id.to_s
+          end
+        end
+      end
+      security.edit_restrictions[:users].each do |privilegedUsers|
+        privilegedUsers.each do |privilegedUser|
+          if privilegedUser == user.id.to_s
+            can :update, Asset, :asset_type_id => security.asset_type_id.to_s
+          end
+        end
+      end
+      security.view_restrictions[:users].each do |privilegedUsers|
+        privilegedUsers.each do |privilegedUser|
+          if privilegedUser == user.id.to_s
+            can :read, Asset, :asset_type_id => security.asset_type_id.to_s
+          end
+        end
+      end
+      security.delete_restrictions[:users].each do |privilegedUsers|
+        privilegedUsers.each do |privilegedUser|
+          if privilegedUser == user.id.to_s
+            can :destroy, Asset, :asset_type_id => security.asset_type_id.to_s
+          end
+        end
+      end
+    end
+
   end
 end
