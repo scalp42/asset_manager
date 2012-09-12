320ed1b4214b47490273a131b4110dc9aaf15338
Gurpreet Parmar <parmarg@gmail.com>
2012-09-11 20:58:35 -0700

    removed default mail server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,5 +36,4 @@ AssetManager::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.smtp_settings = {:address => 'mail1.medu.com' ,:port => 25}
 end
