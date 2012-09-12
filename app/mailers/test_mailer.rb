07bdcddb0ae2f5e2bee85bbd1209c49849a7415a
Gurpreet Parmar <parmarg@gmail.com>
2012-09-11 21:21:15 -0700

    minro update

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,17 +1,20 @@
 class TestMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def test_email(email_address)
-    mail = MailSetting.first()
-
-    ActionMailer::Base.smtp_settings = {
-    :address => mail.server,
-    :port => mail.port,
-    :authentication => :plain,
-    :user_name => mail.username,
-    :password => mail.password
+  def load_settings(mail)
+    @@smtp_settings = {
+      :address              => mail.server,
+      :port                 => mail.port,
+     # :domain               => options["domain"],
+      :authentication       => :plain,
+      :user_name            => mail.username,
+      :password             => mail.password
     }
+  end
+  def test_email(email_address)
 
+    mail = MailSetting.first()
+    load_settings(mail)
     mail(:to => email_address, :subject => "Test Email Asset Manager", :from => mail.from_email)
   end
 end
