a294ecc879521504120f619386dfc127a3694f3c
Gurpreet Parmar <parmarg@gmail.com>
2012-10-03 11:18:10 -0700

    fixed deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,30 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'capistrano_colors'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+capistrano_color_matchers = [
+  { :match => /command finished/,       :color => :hide,      :prio => 10 },
+  { :match => /executing command/,      :color => :blue,      :prio => 10, :attribute => :underscore },
+  { :match => /^transaction: commit$/,  :color => :magenta,   :prio => 10, :attribute => :blink },
+  { :match => /git/,                    :color => :white,     :prio => 20, :attribute => :reverse },
+]
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+colorize( capistrano_color_matchers )
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# =========================================================================
+# CONFIGURATION
+# =========================================================================
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# =======================================================================
+# Application Properties
+# =======================================================================
+set :application, "asset_manager"
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+load 'config/shared/config'
+load 'config/shared/custom_config'
+
+namespace :deploy do
+
+  task :default do
+    sudo 'ls -larth'
+  end
+
+end
