6de6f382c7d5f4f0c1db6b1008075eb3bb387ffb
Gurpreet Parmar <parmarg@gmail.com>
2012-10-03 23:07:28 -0700

    minor fix to allow for production env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :unless => :devise_controller?
 
   protect_from_forgery
 end
